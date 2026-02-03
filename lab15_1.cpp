7b5beb3aded8ef309a5bd72a3e0be8a93bda7f01
prakao <kpkrisrin@gmail.com>
2026-02-03 15:08:01 +0700

    15_1_finished

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,24 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
 
-	
-	
+	int a =5;
+	char b='A';
+	char &c=b;
+	int *pX=&a;
+	char *pY=&b;
+	int *pZ=pX;
+
+	cout<<a<<" "<<b<<" "<<c<<" "<<pX<<" "<<(int *)pY<<" "<<pZ<<" "<<"\n";
+	cout<<&a<<" "<<(int *)&b<<" "<<(int *)&c<<" "<<&pX<<" "<<&pY<<" "<<&pZ<<" "<<"\n";
+	c='F';
+	cout<<a<<" "<<b<<" "<<c<<" "<<pX<<" "<<(int *)pY<<" "<<pZ<<" "<<"\n";
+	*pY='W';
+	cout<<a<<" "<<b<<" "<<c<<" "<<pX<<" "<<(int *)pY<<" "<<pZ<<" "<<"\n";
+	*pX=6;
+	cout<<a<<" "<<b<<" "<<c<<" "<<pX<<" "<<(int *)pY<<" "<<pZ<<" "<<"\n";
+	*pZ=7;
+	cout<<a<<" "<<b<<" "<<c<<" "<<pX<<" "<<(int *)pY<<" "<<pZ<<" "<<"\n";
 	return 0;
 }
